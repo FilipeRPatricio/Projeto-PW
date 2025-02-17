a90ee60c4e30795b1336fa04119cd94a00b3d586
tmCC257 <tiagocampos257@gmail.com>
2025-02-17 16:07:43 +0000

    Delete favorite documentação

--- routes/favorite-types/delete-favorite.js
+++ routes/favorite-types/delete-favorite.js
@@ -4,6 +4,12 @@ import { sendResponse, sendError, toNumber } from "../../connection/database.js"
 
 const deleteTypeCommand = "delete from `FavoriteType` where `Member` = ? and `EventType` = ?";
 
+/**
+ * Apaga o tipo de evento favorito com o id de membro e de tipo pedido.
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
 export default async function deleteFavorite(request, response) {
     const member = toNumber(request.body.member);
     const eventType = toNumber(request.body.eventType);
